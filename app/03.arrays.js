5c60239c61b28a3efc348cc9d421fa6275ab8294
Stanislav Starodubets <stanislav_starodubets@epam.com>
2021-11-11 12:32:38 +0200

    arrays practice added

--- app/03.arrays.js
+++ app/03.arrays.js
@@ -1,3 +1,137 @@
+
+
+// Task 1
+const a1 = [1, 2, 3];
+const a2 = a1;
+a1[0] = 99;
+console.log(a2[0]);
+// Answer: 99
+
+// Task 2
+const entries = ["a", "b", "c"];
+console.log(Array.isArray(entries));
+// Answer: true
+
+// Task 3
+const entries1 = ["a", "b", "c"];
+entries1.length = 10;
+console.log(entries1.length);
+// Answer: 10
+
+// Task 4
+const entries2 = ["a", "b", "c"];
+console.log(entries2.toString());
+// Answer: a,b,c
+
+// Task 5
+const entries3 = [];
+console.log(entries3.pop());
+// Answer: undefined
+
+// Task 6
+const entries4 = ["a", "b", "c", "d"];
+const newEntries4 = entries4.slice(2);
+console.log(newEntries4);
+// Answer: ["c", "d"];
+
+// Task 7
+const entries5 = ["a", "b", "c", "d"];
+const r = entries5.splice(2, 1, "A", "B", "C");
+console.log(entries5.toString());
+console.log(r);
+// Answer: a,b,A,B,C,d;
+// Answer: ["c"];
+
+// Task 8
+const ratings = [1, 2, 3, 4, 5, 4, 3, 2, 1];
+console.log(ratings.indexOf(66));
+// Answer: -1
+
+// Task 9
+const entries6 = ["a", "b", "c", "d"];
+entries6.forEach(
+   function(element, index, array) {
+     if (index === 1) array.push("e");
+	console.log(element.toUpperCase());
+});
+// Answer: A,B,C,D;
+
+// Task 10
+const ratings2 = [1, 2, 3, 4, 5];
+const result = ratings2.reduce((start, next, index, array) => start * next); 
+console.log(result);
+// Answer: 120
+
+// Task 11
+const priority = [1, 2, 3];
+const priorityCopy = Array.from(priority);
+console.log(priority === priorityCopy);
+// Answer: false
+
+// Task 12
+const priority2 = [1, 2, 3];
+const result2 = priority2.find(elem => elem >= 2);
+console.log(result2);
+// Answer: 2
+
+// Task 13
+const priority3 = [1, 2, 3];
+const result3 = priority3.includes(2);
+console.log(result3);
+// Answer: true
+
+// Task 14
+const nums = [ 1, 2, [ 3, 4 ], [ [ 5, 6 ] ] ];
+console.log(nums.flat(Infinity));
+// Answer: [ 1, 2, 3, 4, 5, 6 ];
+
+// Task 15
+const priorityCodes = ["A", ..."BC", "D"];
+console.log(priorityCodes);
+// Answer: 
+
+// Task 16
+const priority6 = ["Low", "Normal", "High"];
+const [low, ...remaining] = priority6;
+console.log(remaining);
+// Answer: 
+
+// Task 17
+const priority7 = new Set();
+priority7.add("High");
+priority7.add("Low");
+console.log(priority7.size);
+// Answer:
+
+// Task 18
+const priority8 = new Set(["High","Low"]);
+function logSetElements(key, value, set) {
+    console.log(`s[${key}] = ${value}`);
+}
+priority8.forEach(logSetElements);
+// Answer:
+
+// Task 19
+const task1 = { action: "Create…" };
+const task2 = { action: "Delete…" };
+const project = new WeakSet([task1, task2]);
+console.log(project.has(task1));
+// Answer: 
+
+// Task 20
+const task11 = { action: "Create…" };
+const task22 = { action: "Delete…" };
+const project2 = new WeakSet([task11, task22]);
+// for (let p of project2) {	
+//     console.log(p.action);
+// }
+// Answer: 
+
+
+
+
+
+
 console.log('Topic: Arrays');
 
 // Task 01
@@ -10,6 +144,12 @@ console.log('Topic: Arrays');
 //     2.    with deleting this element from an array.
 //     Display them in the console.
 
+
+const arr1 = [1,2,3,4]
+console.log(arr1[arr1.length -1 ]);
+console.log(arr1.pop());
+
+
 // Task 02
 // RU: Создать массив любых элементов. Добавить элемент в конец массива.
 //     1. Модифицировать текущий массив
@@ -20,18 +160,40 @@ console.log('Topic: Arrays');
 //     2. create a new array.
 //     Disply them in the conole.
 
+const arr2 = [1,2,3,4]
+arr2.push(5)
+console.log(arr2);
+
+const arr2_1 = [...arr2, 5]
+console.log(arr2);
+console.log(arr2_1);
+
+
+
 // Task 03
-// RU: Создать массив любых элементов. Вставить новый элемент под индексом 3.
+// RU: Создать массив любых элементов. Вставить новый элемент под индексом 2.
 //     1. Модифицировать текущий массив
 //     2. Создать новый массив
 //     Выведите массивы в консоль
-// EN: Create an array of any elements. Insert a new element with index 3.
+// EN: Create an array of any elements. Insert a new element with index 2.
 //     1. mutate current array;
 //     2. create a new array.
 //     Disply them in the conole.
 
+const arr3 = [1,2,3,4]
+const arr3_1 = [1,2,3,4]
+
+arr3.splice(2, 0, 5)
+arr3_1.splice(2, 1, 5)
+
+console.log(arr3);
+console.log(arr3_1);
 
 
+const arr3_2 = [1,2,3,4]
+
+const arr3_2_new = [...arr3_2.slice(0,3), '23', ...arr3_2.slice(3)]
+console.log(arr3_2_new);
 
 // Task 04
 // RU: Создать массив любых элементов.
@@ -39,17 +201,46 @@ console.log('Topic: Arrays');
 // EN: Create an array of any elements.
 //     Iterate over this array and display each element in the console.
 
+
+const arr4 = [1,2,3,4]
+
+arr4.forEach(item => console.log(item))
+
+for (const a of arr4.entries()) {
+  console.log(a);
+}
+
+for (const a of arr4.keys()) {
+  console.log(a);
+}
+for (const a of arr4.values()) {
+  console.log(a);
+}
+
+
+
 // Task 05
 // RU: Создать массив чисел в диапазоне от 0 до 100.
 //     Подсчитать и вывети сумму тех элементов, которые больше 50.
 // EN: Create an array of numbers in the range from 0 to 100.
 //     Calculate and display the sum of the elements, which are greater than 50.
 
+
+const arr5 = [1,100,100,100,33,23]
+
+const sum = arr5.filter((item) => item > 50).reduce((acc, item) => acc + item)
+console.log(sum);
+
 // Task 06
 // RU: Создать массив строк. На основе этого массива создать строку –
 //     объдинить все элементы массива, используя определенный разделитель.
 // EN: Create an array of strings. Create a string on the basis of this array.
 //     This string should contain all elements from an array separated by certain delimeter.
+
+
+const arr6 = ['A', 'B', 'C']
+
+console.log(arr6.join('__'));
 
 // Task 07
 // RU: Создать массив чисел от 1 до 20 в случайном порядке.
@@ -58,6 +249,13 @@ console.log('Topic: Arrays');
 // EN: Create an array of numbers in the range from 1 to 20 in random order.
 //     Sort this array in ascending order. Display it in the console.
 //     Create a copy of the previous array in reverse order. Disply it in the console.
+
+
+const arr7 = [1,12,4,3,14,16,6]
+
+console.log(arr7.sort((a, b) => a - b));
+console.log(arr7.sort((a, b) => b - a));
+
 
 // Task 08
 // RU: Создать массив [3, 0, -1, 12, -2, -4, 0, 7, 2]
@@ -70,6 +268,18 @@ console.log('Topic: Arrays');
 //     First part - negative values from the original array in the same order,
 //     Next part - zeroes
 //     Last part - positive values from the original array in the same order.
+
+const arr8 = [3, 0, -1, 12, -2, -4, 0, 7, 2]
+  const arr8_1 = arr8.filter(item => item < 0)
+  const arr8_2 = arr8.filter(item => item === 0 )
+  const arr8_3 = arr8.filter(item => item > 0)
+  console.log(arr8_1);
+  console.log(arr8_2);
+  console.log(arr8_3);
+
+  const summaryArr8 = [...arr8_1, ...arr8_2, ...arr8_3]
+  console.log(summaryArr8);
+
 
 // Task 09
 // RU: 1. Создайте массив styles с элементами "Jazz", "Blues".
@@ -85,17 +295,56 @@ console.log('Topic: Arrays');
 //     5. Add two new elements "Rap" and "Reggae" at the begining of the array.
 //     6. Display an array in the console.
 
+
+const styles = ['Jazz', 'Blues']
+styles.push("Rock-n-Roll")
+console.log(styles);
+
+styles.splice(-2, 1, 'Classics')
+
+console.log(styles);
+
+console.log(styles.shift())
+
+console.log(styles);
+
+styles.unshift('Rap', 'Reggae')
+
+console.log(styles);
+
+
+
+
 // Task 10
 // RU: Подсчитать в строке "dskjdhfkjshdfkjhsdkjureyteiruyiqywehjkh"
 //     отдельно количество букв r, k, t и вывести в консоль.
 // EN: Calculate the number of letters r, k, t in this string
 //     "dskjdhfkjshdfkjhsdkjureyteiruyiqywehjkh" and display them in the console.
 
+
+const str10 = 'dskjdhfkjshdfkjhsdkjureyteiruyiqywehjkh'
+
+const count = str => {
+  return [...str].reduce((acc, item) => {
+    if(item === 'r' || item === 'k' || item === 't') {
+      (acc[item] = (acc[item] || 0) + 1)
+    }
+     return acc
+  },{})
+}
+
+console.log(count(str10));
+
+
 // Task 11
 // RU: Создать массив любых элементов.
 //     Получить случайный элемент из массива и вывести его в консоль.
 // EN: Create an array of any elements.
 //     Get the random element from this array and display it in the console.
+
+
+
+
 
 // Task 12
 // RU: Создать двумерный массив:
@@ -109,11 +358,32 @@ console.log('Topic: Arrays');
 //     7 8 9
 //     Display it in the console.
 
+
+const arr12 = [
+  [1,2,3],
+  [1,2,3],
+  [1,2,3],
+]
+
+for (const some of arr12)
+{console.table(some.join(''))}
+
+
+
 // Task 13
 // RU: Преобразовать массив из предыдущего задания в одномерный.
 //     Вывести его в консоль
 // EN: Transform an array from the previous task into one-dementional array.
 //     Display it in the console.
+
+const arr13 = [
+  [1,2,3],
+  [1,2,3],
+  [1,2,3],
+  [[[[1,2,3,4]]]]
+]
+console.log(arr13.flat(Infinity));
+
 
 // Task 14
 // RU: Создать массив любых элементов.
@@ -125,11 +395,18 @@ console.log('Topic: Arrays');
 //     contain elements from index a to index b.
 //     Display it in the console.
 
+
+// slice
+
 // Task 15
 // RU: Создать массив любых элементов.
 //     Найти индекс указаного элемента в массиве и вывести его в консоль.
 // EN: Create an array of any elements.
 //     Find the index of a particular element in the array and display it in the console.
+
+
+//indexOf
+//findIndex
 
 // Task 16
 // RU: Создать массив с дублями элементов. На его основе создать новый массив
@@ -139,6 +416,9 @@ console.log('Topic: Arrays');
 //     Remove duplicated elements.
 //     Display it in the console.
 
+
+
+
 // Task 17
 // RU: Создать массив с дублями. Найти первый элемент, который дублируется.
 //     Заменить этот элемент и все его копии на символ '*'.
@@ -146,6 +426,23 @@ console.log('Topic: Arrays');
 // EN: Create an array with duplicate elements. Find first duplicated element.
 //     Replace this element and all its copies with symbol '*'.
 //     Display it in the console.
+
+const arr17 = [1,3,2,3,4,3,4,5,3,3,2]
+
+for (let el of arr17) {
+  if (arr17.indexOf(el) !== arr17.lastIndexOf(el)) {
+    arr17.forEach((e, index, arr) => {
+      if (e === el) {
+        arr[index] = "*"
+      }
+    })
+    break
+  }
+}
+
+console.log(arr17);
+
+
 
 // Task 18
 // RU: Создать массив целых чисел. На его основе создать массивы – представления
@@ -156,6 +453,9 @@ console.log('Topic: Arrays');
 //     Second array contains the octal representation of the elements from the original array.
 //     Third array contains the hexadecimal representation of the elements from the original array.
 //     Display them in the console.
+
+
+
 
 // Task 19
 // RU: Получить из строки 'a big brown fox jumps over the lazy dog' массив слов,
--- app/07.promises.js
+++ app/07.promises.js
@@ -93,3 +93,231 @@ console.log('Topic: Promises');
 // Создайте два промиса. Первый промис возвращает объект { name: "Anna" } через 2с,
 // Второй промис возвращает дефолтный объект { name: "Unknown" } через 1с.
 // Запустите оба эти промисы в параллель и получите результат того, который отработает первым
+
+
+
+
+//==========================================================
+
+//PROMISES AFTER CLASS
+
+console.log('Topic: Async Functions');
+
+
+
+async function asyncFunction1111() {   // async всегда возвращает промис
+  // await лвозвращает результат из промиса
+  ;
+}
+
+
+async function asyncF1() {
+  console.log('async FDS');
+}
+const asyncProto = Object.getPrototypeOf(asyncF1);
+console.log(asyncProto); // {AsyncFunction}
+console.log(asyncF1[Symbol.toStringTag]) // AsyncFunction
+
+// Task 01
+// RU: Создайте асинхронную функцию f1, используя FDS (Function Declaration Statement).
+//     Функция должна принимать два параметра a и b и возвращать сумму a+b.   
+//     Выведите значение, которое вернет функция в консоль.
+//     Обработаете промис и выведите значение в консоль.
+// EN: Create an async function f1 as a Function Declaration Statement.
+//     The function should take two parameters a and b and return a sum a+b.
+//     Display the result of function in the console.
+//     Process a promise and display value in the console.
+
+
+
+async function f11(a, b) {
+  return (a + b);
+}
+console.log(f11(2, 3));
+f11(2, 3).then(res => console.log(res));
+
+// Task 02
+// RU: Создайте асинхронную функцию f2, используя FDE (Function Definition Expression).
+//     Функция должна возвращать 'Promise Data', используя Promise.resolve()
+//     Выведите значение, которое вернет функция в консоль.
+//     Обработаете промис и выведите значение в консоль.
+// EN: Create an async function f2 as a Function Definition Expression.
+//     The function should return the string 'Promise data' using Promise.resolve() method.
+//     Display the result of function in the console.
+//     Process a promise and display value in the console.
+
+const someFunc2 = async () => {
+  return Promise.resolve('Promise Data')
+}
+
+console.log(someFunc2());
+someFunc2().then(res => console.log(res))
+
+
+// Task 03
+// RU: Создайте класс C1. Добавьте асинхронный метод f3.
+//     Метод должен генерить исключение 'Error occurs in f3 method'.
+//     Создайте экземпляр класса и вызовите метод f3.
+//     Обработаете промис.
+// EN: Create a class C1. Add async method f3.
+//     Method should throw an exception 'Error occurs in f3 method'.
+//     Create an instance of the class and call the method f3.
+//     Process a promise and display value in the console.
+
+class C1 {
+  async asyncMethod() {
+    throw new Error('error occurs in this method')
+  }
+}
+
+const c1Instance = new C1;
+c1Instance.asyncMethod().catch(e => console.log(e))
+
+// Task 04
+// RU: Cоздайте функцию makeRequest, используя FDS (Function Declaration Statement).
+//     Функция должна принимать один параметр - url и возвращать промис,
+//     который перейдет в состояние resolved через 2с. и вернет значение параметра.
+//     Первой строкой в функции выведите сообщение 'makeRequest is called'.
+//     Cоздайте функцию f4, используя FDS (Function Declaration Statement).
+//     Функция должна вызвать функцию makeRequest, получить результат ее работы и вывести в консоль.
+// EN: Create a functiom makeRequest as a Function Declaration Statement.
+//     The function should take one parameter - url and return a promise,
+//     which should be resolved after 2s and it should return the value of the parameter url.
+//     The first line of code in the function should display message 'makeRequest is called' in the console.
+//     Create a function f4 as a Function Declaration Statement.
+//     The function f4 should call the function makeRequest, get its result and display it in the console.
+
+function makeRequest(url) {
+  console.log('makeRequest is called');
+  return new Promise(resolve => {
+    // setTimeout(resolve, 2000, url)
+    setTimeout(() => {
+      resolve(url)
+    }, 2000)
+  })
+}
+
+function f4(url) {
+  return makeRequest(url)
+}
+
+console.log(f4('http://example.com'))
+
+
+// Task 05
+// RU: Внесите изменения в функцию f4 из предыдущего задания так, чтобы в консоле появилось
+//     значение переданого параметра в функцию makeRequest.
+// EN: Make changes to the function f4 from the previous task. This function should display the value
+//     of the parameter of the function makeRequest in the console.
+
+function makeRequest2(url) {
+  console.log('makeRequest is called');
+  return new Promise(resolve => {
+    // setTimeout(resolve, 2000, url)
+    setTimeout(() => {
+      resolve(url)
+    }, 2000)
+  })
+}
+
+function f42(url) {
+  return makeRequest(url).then(console.log(url))
+}
+f42('http://example.com')
+
+// Task 06
+// RU: Cоздайте асинхронную функцию f6, используя FDS (Function Declaration Statement).
+//     Функция должна вызвать два раза функцию makeRequest с разными значениями параметра.
+//     Перед вызовом и после каждого вызова makeRequest функция должна выводить в консоль любое сообщение.
+//     Создать и вернуть массив, который должен содержать значения переданые функции makeRequest.
+//     Обработаете результат работы функции f6.
+// EN: Create an async function f6 as a Function Declaration Statement.
+//     This function should call the function makeRequest two times with different values of its parameter.
+//     The function f6 should display any message in the console before and after each call 
+//     of the function makeRequest.
+//     Create and return the array from the function f6, which should contains the values of the 
+//     parameter of the function makeRequest.
+//     Process the result of the function f6.
+
+async function f6(url1, url2) {
+  console.log('first message');
+  await makeRequest2(url1);
+  console.log('second message');
+  await makeRequest2(url2);
+  console.log('third message');
+  let res = [];
+  res.push(url1);
+  res.push(url2);
+  return res;
+}
+
+console.log(f6('url-1', 'url-2').then(console.log))
+
+
+
+// Task 07
+// RU: Измените асинхронную функцию f6 из предыдущего задания так, чтобы вызовы функции
+//     makeRequest выполнялись паралельно.
+// EN: Make changes to the async function f6 from the previous task. This function should 
+//     call the function makeRequest with different values of its parameter simultaneously.
+
+
+function f62(url1, url2) {
+  return Promise.all([makeRequest2(url1), makeRequest2(url2)])
+  // return Promise.allSettled([makeRequest2(url1), makeRequest2(url2)])
+}
+
+console.log(f62('url-1', 'url-2').then(console.log))
+
+
+
+// Task 08
+// RU: Создайте масив урлов ['http://a', 'http://b'].
+//     Создайте функцию sendRequest, которая принимает один параметр - url.
+//     Функция в первой строке должна выводить в консоль сообщение 'sendRequest is called'
+//     и возвращать через 2 с для первого урла объект { name: 'Ann' }, а для второго
+//     урла объект { age: 16 }.
+//     Создайте асинхронную функцию f8 которая должна вызвать функцию sendRequest для каждого урла
+//     и вернуть объект {name: 'Ann', age: 16}
+//     Обработайте результат работы функции f8
+// EN: Create the array of urls ['http://a', 'http://b'].
+//     Create the function sendRequest which should take one parameter - url.
+//     The function should display the message 'sendRequest is called' in the console 
+//     in its first line of code. Then the function should return the object { name: 'Ann' } for the 
+//     first url after 2s and the object { age: 16 } for the second url after 2s.
+//     Create the async function f8, which should call the function sendRequest with each value 
+//     from the array and return the object {name: 'Ann', age: 16}.
+//     Process the reuslt of the function f8
+
+
+function sendRequest(url) {
+
+  const map = new Map([
+    ['http://a', { name: 'Ann' }],
+    ['http://b', { age: 16 }]
+  ]);
+
+  console.log('sendRequest is called');
+
+  return new Promise((resolve => {
+    setTimeout(resolve, 2000, map.get(url));
+  }))
+}
+
+async function f8() {
+  const urls = ['http://a', 'http://b'];
+
+  const arrPromises = urls.map(url => sendRequest(url));
+
+  const result = await Promise.all(arrPromises);
+
+  return result.reduce((acc, item) => {
+    console.log(acc);
+    console.log(item);
+    return { ...acc, ...item }
+  }, {})
+}
+
+
+f8().then(console.log)
+
